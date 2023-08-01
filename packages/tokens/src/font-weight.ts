cf16e30791155056d7fed7874b0fcd09ea71c45f
João Victor <joaovr.costa@gmail.com>
2023-07-31 23:10:29 -0300

    fix: fixed token's name

--- packages/tokens/src/font-weight.ts
+++ packages/tokens/src/font-weight.ts
@@ -1,4 +1,4 @@
-export const fontWeights = {
+export const FontWeights = {
   regular: '400',
   medium: '500',
   bold: '700',
--- packages/tokens/src/fonts.ts
+++ packages/tokens/src/fonts.ts
@@ -1,4 +1,4 @@
-export const fonts = {
+export const Fonts = {
   default: 'Roboto, sans-serif',
   code: 'monospace',
 }
